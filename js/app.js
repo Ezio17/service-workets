a522d1de241d89c7023445f882fa4533040aab9d
Mykola Shcherba <xd_ezio@ukr.net>
2023-02-13 17:38:39 +0200

    init

--- js/app.js
+++ js/app.js
@@ -1,34 +1,46 @@
-window.addEventListener('load', async () => {
-  if ('serviceWorker' in navigator) {
-    try {
-      const reg = await navigator.serviceWorker.register('/sw.js')
-      console.log('Service worker register success', reg)
-    } catch (e) {
-      console.log('Service worker register fail')
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+  async function start() {
+    console.log(555, navigator);
+    if ("serviceWorker" in navigator) {
+      try {
+        const reg = await navigator.serviceWorker.register("/sw.js");
+        console.log("Service worker register success", reg);
+      } catch (e) {
+        console.log("Service worker register fail");
+      }
     }
+
+    await loadPosts();
   }
 
-  await loadPosts()
-})
+  start();
 
+  const h3 = document.querySelector("h3");
 
-async function loadPosts() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=11')
-  const data = await res.json()
+  h3.innerText = window.navigator.onLine ? "online" : "offline";
 
-  const container = document.querySelector('#posts')
-  container.innerHTML = data.map(toCard).join('\n')
-}
+  async function loadPosts() {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/posts?_limit=11"
+    );
+    const data = await res.json();
 
-function toCard(post) {
-  return `
-    <div class="card">
-      <div class="card-title">
-        ${post.title}
+    const container = document.querySelector("#posts");
+    container.innerHTML = data.map(toCard).join("\n");
+  }
+
+  function toCard(post) {
+    return `
+      <div class="card">
+        <div class="card-title">
+          ${post.title}
+        </div>
+        <div class="card-body">
+          ${post.body}
+        </div>
       </div>
-      <div class="card-body">
-        ${post.body}
-      </div>
-    </div>
-  `
+    `;
+  }
 }
